bec845b17c6fbb29ec6657b1d8f36f3217dfab44
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-06 13:34:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,19 +1,19 @@
-
 class Shoe
-attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  
+  BRANDS = []
 
-BRANDS = []
-def initialize(brand)
-  # @brand = brand
-end
+  def initialize(brand)
+    # @brand = brand
+  end
 
-def cobble
-  self.condition = "new"
-  puts "Your shoe is as good as new!"
-end
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 
-def brand=(brand)
-  @brand = brand
-  BRANDS << brand
-end
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand 
+  end
 end
