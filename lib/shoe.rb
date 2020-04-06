460d0ea5fe2bb17f5a7799cfd982b49bbc9270a4
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-06 13:34:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  
+
   BRANDS = []
 
   def initialize(brand)
@@ -14,6 +14,6 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    BRANDS << brand 
+    BRANDS << brand
   end
 end
